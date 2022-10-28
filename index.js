87e6c1f056c4aec3e77a5ccbb438d2a18f987965
Nishant-nehra <57745006+Nishant-nehra@users.noreply.github.com>
2022-10-28 22:02:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import stylesheets
+import './style.css';
+
+// var counter = 1;
+
+// function AddHtmlElement(text) {
+//   let divElement = document.getElementById('itemList');
+//   let newdiv = document.createElement('div');
+//   newdiv.setAttribute('id', counter);
+//   divElement.appendChild(newdiv);
+//   newdiv.innerHTML = `<div class="itemListEl">
+//   ${text}<a href='#' onclick=removeListItem(${counter})>X</a>
+// </div>
+//   `;
+//   counter += 1;
+// }
+
+// function removeListItem(divNum) {
+//   let divId = document.getElementById('itemList');
+//   let childEl = document.getElementById(divNum);
+//   divId.removeChild(childEl);
+// }
